2fb36887d7494f090ad6a76257dd97c619e6c9f6
Eilis Byrne <eilis@hoistapps.com>
2014-11-28 15:24:43 +1300

    load connector by only key, or both key and type

--- tests/unit_tests/connector_tests.js
+++ tests/unit_tests/connector_tests.js
@@ -6,80 +6,109 @@ var expect = require('chai').expect;
 var errors = require('hoist-errors');
 var BBPromise = require('bluebird');
 
-describe('Hoist', function () {
+describe.only('Hoist', function () {
   describe('.connector', function () {
-    var connector;
     var StubConnector = function () {
       this.get = sinon.stub();
       this.put = sinon.stub();
       this.post = sinon.stub();
     };
+    var connector;
     var stubConnector = new StubConnector();
-    before(function () {
-      sinon.stub(ConnectorPipeline.prototype, 'loadConnector').returns(BBPromise.resolve(stubConnector));
-      connector = Hoist.connector('xero', 'key');
-    });
-    it('loads the specified connector', function () {
-      expect(ConnectorPipeline.prototype.loadConnector)
-        .to.have.been.calledWith('xero', 'key');
-    });
-    it('rejects if no connector type specified', function () {
-      expect(function () {
-        Hoist.connector(null, 'key');
-      }).to.throw(errors.connector.request.InvalidError);
-    });
-    it('rejects if no connector key specified', function () {
-      expect(function () {
-        Hoist.connector('xero');
-      }).to.throw(errors.connector.request.InvalidError);
-    });
+    describe('with type and key', function (){
+      before(function () {
+        sinon.stub(ConnectorPipeline.prototype, 'loadConnector').returns(BBPromise.resolve(stubConnector));
+        connector = Hoist.connector('xero', 'key');
+      });
+      after(function(){
+        ConnectorPipeline.prototype.loadConnector.restore();
+      });
+      it('loads the specified connector', function () {
+        expect(ConnectorPipeline.prototype.loadConnector)
+          .to.have.been.calledWith('xero', 'key');
+      });
+      describe('#get', function () {
+        var response;
+        var _promise = BBPromise.resolve(true);
+        before(function () {
+          stubConnector.get.returns(_promise);
+          response = connector.get('/path?query');
+        });
+        after(function () {
+          stubConnector.get.reset();
+        });
+        it('calls pipeline#get', function () {
+          expect(stubConnector.get)
+            .to.have.been.calledWith('/path?query');
+        });
+      });
+      describe('#put', function () {
+        var response;
+        var _promise = BBPromise.resolve(true);
+        before(function () {
+          stubConnector.put.returns(_promise);
+          response = connector.put('/path?query', 'data');
+        });
+        after(function () {
+          stubConnector.put.reset();
+        });
+        it('calls pipeline#put', function () {
+          expect(stubConnector.put)
+            .to.have.been.calledWith('/path?query', 'data');
+        });
+      });
+      describe('#del', function () {
 
-    describe('#get', function () {
-      var response;
-      var _promise = BBPromise.resolve(true);
-      before(function () {
-        stubConnector.get.returns(_promise);
-        response = connector.get('/path?query');
       });
-      after(function () {
-        stubConnector.get.reset();
-      });
-      it('calls pipeline#get', function () {
-        expect(stubConnector.get)
-          .to.have.been.calledWith('/path?query');
-      });
-    });
-    describe('#put', function () {
-      var response;
-      var _promise = BBPromise.resolve(true);
-      before(function () {
-        stubConnector.put.returns(_promise);
-        response = connector.put('/path?query', 'data');
-      });
-      after(function () {
-        stubConnector.put.reset();
-      });
-      it('calls pipeline#put', function () {
-        expect(stubConnector.put)
-          .to.have.been.calledWith('/path?query', 'data');
+      describe('#post', function () {
+        var response;
+        var _promise = BBPromise.resolve(true);
+        before(function () {
+          stubConnector.post.returns(_promise);
+          response = connector.post('/path?query', 'data');
+        });
+        after(function () {
+          stubConnector.post.reset();
+        });
+        it('calls pipeline#post', function () {
+          expect(stubConnector.post)
+            .to.have.been.calledWith('/path?query', 'data');
+        });
       });
     });
-    describe('#del', function () {
-
-    });
-    describe('#post', function () {
-      var response;
-      var _promise = BBPromise.resolve(true);
+    describe('with key and null type', function () {
+      var connector;
       before(function () {
-        stubConnector.post.returns(_promise);
-        response = connector.post('/path?query', 'data');
+        sinon.stub(ConnectorPipeline.prototype, 'loadConnector').returns(BBPromise.resolve(stubConnector));
+        connector = Hoist.connector(null, 'key');
       });
-      after(function () {
-        stubConnector.post.reset();
+      after(function(){
+        ConnectorPipeline.prototype.loadConnector.restore();
       });
-      it('calls pipeline#post', function () {
-        expect(stubConnector.post)
-          .to.have.been.calledWith('/path?query', 'data');
+      it('loads the specified connector', function () {
+        expect(ConnectorPipeline.prototype.loadConnector)
+          .to.have.been.calledWith(null, 'key');
+      });
+    });
+    describe('with one argument', function () {
+      var connector;
+      before(function () {
+        sinon.stub(ConnectorPipeline.prototype, 'loadConnector').returns(BBPromise.resolve(stubConnector));
+        connector = Hoist.connector('key');
+      });
+      after(function(){
+        ConnectorPipeline.prototype.loadConnector.restore();
+      });
+      it('loads the specified connector', function () {
+        expect(ConnectorPipeline.prototype.loadConnector)
+          .to.have.been.calledWith('key');
+      });
+    });
+    describe('with no type or key specified', function () {
+      it('rejects', function () {
+        expect(function () {
+          Hoist.connector();
+        }).to.throw(errors.connector.request.InvalidError);
       });
     });
   });
