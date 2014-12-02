eb08515c1120f171853e5daf7aca2bf73d97d4a6
Owen Evans <owen@bgeek.net>
2014-12-02 23:05:23 +1300

    fix tests

--- tests/bootstrap.js
+++ tests/bootstrap.js
@@ -7,20 +7,21 @@ var expect = chai.expect;
 process.env.NODE_ENV = 'test';
 var BBPromise = require('bluebird');
 var unhandledPromises = [];
-BBPromise.onPossiblyUnhandledRejection(function(reason, promise) {
-    unhandledPromises.push(promise);
-    //Update some debugger UI
+BBPromise.onPossiblyUnhandledRejection(function (reason, promise) {
+  console.log(reason.stack);
+  unhandledPromises.push(promise);
+  //Update some debugger UI
 });
 
-BBPromise.onUnhandledRejectionHandled(function(promise) {
-    var index = unhandledPromises.indexOf(promise);
-    unhandledPromises.splice(index, 1);
-    //Update the debugger UI
+BBPromise.onUnhandledRejectionHandled(function (promise) {
+  var index = unhandledPromises.indexOf(promise);
+  unhandledPromises.splice(index, 1);
+  //Update the debugger UI
 });
 
 after(function () {
-  if(unhandledPromises.length>0){
-    console.error('unhandled promise exceptions',unhandledPromises);
+  if (unhandledPromises.length > 0) {
+    console.error('unhandled promise exceptions', unhandledPromises);
 
   }
   expect(unhandledPromises.length).to.eql(0);
