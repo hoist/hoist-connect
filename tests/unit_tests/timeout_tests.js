f895c31eed4a1ece92e304a9bbd2b1664d813249
Owen Evans <owen@bgeek.net>
2014-12-04 13:22:28 +1300

    add tests for timeout
    fixes hoist/Hoist#54

---
+++ tests/unit_tests/timeout_tests.js
@@ -0,0 +1,47 @@
+'use strict';
+require('../bootstrap');
+var Hoist = require('../../lib');
+var expect = require('chai').expect;
+var Context = require('hoist-context');
+var sinon = require('sinon');
+var BBPromise = require('bluebird');
+describe('Hoist.timeout', function () {
+  it('exists', function () {
+    return expect(Hoist).to.have.property('timeout');
+  });
+  describe('#extend', function () {
+    var clock;
+    var context;
+    var result = '';
+    beforeEach(function () {
+      context = {
+        timeout: {
+          onTimeout: function () {
+            result = 'this is a reset timeout';
+          }
+        }
+      };
+      result = '';
+      clock = sinon.useFakeTimers();
+      context.timeout.current = setTimeout(function () {
+        result = 'this is original timeout';
+      }, 300);
+      sinon.stub(Context, 'get').returns(BBPromise.resolve(context));
+      clock.tick(200);
+      return Hoist.timeout.reset(5000);
+    });
+    it('replaces timeout', function () {
+      clock.tick(200);
+      return expect(result).to.eql('');
+    });
+    it('sets new timeout', function () {
+      clock.tick(5000);
+      return expect(result).to.eql('this is a reset timeout');
+    });
+    afterEach(function () {
+      clearTimeout(context.timeout.current);
+      Context.get.restore();
+      clock.restore();
+    });
+  });
+});
