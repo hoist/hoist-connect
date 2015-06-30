459da04894792ac167a0d46d392f0f7b4a14293e
Owen Evans <owen@bgeek.net>
2015-06-30 19:40:17 +1200

    update data api
    and :bookmark: npm bump

--- tests/unit_tests/data_find_tests.js
+++ tests/unit_tests/data_find_tests.js
@@ -4,7 +4,7 @@ var Hoist = require('../../src');
 var expect = require('chai').expect;
 var sinon = require('sinon');
 var BBPromise = require('bluebird');
-var DataPipeline = require('@hoist/data-pipeline').Pipeline;
+var DataPipeline = require('@hoist/data-pipeline');
 
 /** @test {DataAPI} */
 describe('Hoist.data', function () {
