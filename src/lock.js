7f911ef75f0d8ef69c78bd3597d573f7a2d1e19f
Owen Evans <owen@bgeek.net>
2015-12-07 13:21:51 +1300

    make lock use redis cluster

--- src/lock.js
+++ src/lock.js
@@ -2,6 +2,7 @@
 import config from 'config';
 import redisLock from 'redis-lock';
 import redis from 'redis';
+import redisSentinel from 'redis-sentinel-client';
 import BaseAPI from './base_api';
 
 let client;
@@ -40,9 +41,20 @@ class HoistLockAPI extends BaseAPI {
   }
 }
 
+function createClient() {
+  if (config.has('Hoist.redis.clustered') && config.get('Hoist.redis.clustered')) {
+    return redisSentinel.createClient({
+      host: config.get('Hoist.redis.host'),
+      port: config.get('Hoist.redis.port'),
+      masterName: config.get('Hoist.redis.masterName')
+    });
+  } else {
+    return redis.createClient(config.get('Hoist.redis.port'), config.get('Hoist.redis.host'));
+  }
+}
 
 HoistLockAPI._getClient = function () {
-  return client || (client = redis.createClient(config.get('Hoist.redis.port'), config.get('Hoist.redis.host')));
+  return client || (client = createClient());
 };
 HoistLockAPI._clearClient = function () {
   if (client) {
